21472004394400c0036d3791ef20c9c397f7c0a5
dengjj <18993882884@163.com>
2024-12-18 11:13:38 +0800

    feat: init + cube01

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Cube from "@/components/cube.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Cube />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/cube.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="cube-container">
+    <canvas width="400" height="400" id="three" />
+  </div>
+</template>
+
+<script setup>
+import * as THREE from "three";
+
+import { onMounted } from "vue";
+
+let cube = null;
+let scene = null;
+let renderer = null;
+let camera = null;
+
+function initScene() {
+  scene = new THREE.Scene(); // 创建场景
+
+  const fov = 90; // 视野范围（透视相机使用度，其他使用弧度）
+  const aspect = 2; // 画布宽高比
+  const near = 0.1; // 近平面
+  const far = 5; // 远平面
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const canvas = document.getElementById("three");
+  renderer = new THREE.WebGLRenderer({
+    canvas, // 渲染的画布
+    antialias: true, // 开启抗锯齿
+  }); // 创建渲染器
+
+  // renderer.setAnimationLoop(animate);
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth); // 创建立方体
+
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // 创建材质
+
+  cube = new THREE.Mesh(geometry, material); // 创建网格（包含几何体、材质）
+
+  scene.add(cube); // 将网格添加到场景
+
+  // renderer.render(scene, camera); // 将场景和相机传递给渲染器来渲染
+
+  function render(time) {
+    time *= 0.001; // 将时间单位变为秒
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+onMounted(() => {
+  initScene();
+});
+</script>
